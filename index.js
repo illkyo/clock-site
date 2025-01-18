0e405e6495e60bd6332f4aee37184fd240d22c7b
illkyo <82893863+4thjonasbrother@users.noreply.github.com>
2025-01-18 14:18:21 +0500

    feat: add new javascript function to make clock

--- index.js
+++ index.js
@@ -4,27 +4,69 @@ const colorBtn = document.querySelector('.color-btn');
 
 let stopped = false;
 
-function updateClock(hour) {
-  hourHand.classList.remove(hourHand.classList.item(1));
-  let addClass = 'hour-' + ((hour > 12) ? (hour - 12).toString() : hour.toString());
-  hourHand.classList.add(addClass);
+function makeClock() {
+  const clockOuter = document.querySelector('.clock-outer');
+  const radius = clockOuter.offsetWidth / 2;
+
+  for(let i = 1; i <= 60; i++) {
+    const item = document.createElement('div');
+    item.classList.add('item');
+
+    if (i % 5 === 0) {
+      const line = document.createElement('span');
+      line.classList.add('number-line');
+
+      const number = document.createElement('h1');
+      number.textContent = i / 5;
+      number.classList.add('number');
+
+      item.appendChild(line);
+      item.appendChild(number);
+
+      clockOuter.appendChild(item);
+
+      const rotation = 360/60;
+
+      item.style.transform = `rotate(${i*rotation}deg) translate(${radius-40}px) rotate(90deg)`;
+      number.style.transform = `rotate(${-(i*rotation)}deg)`;
+
+    } else {
+      const line = document.createElement('span');
+      line.classList.add('line');
+  
+      item.appendChild(line);
+      clockOuter.appendChild(item);
+
+      const rotation = 360/60;
+
+      item.style.transform = `rotate(${i*rotation}deg) translate(${radius-15}px) rotate(90deg)`;
+    }
+  }
 }
 
-updateClock(new Date().getHours());
+makeClock();
 
-let intervalId = setInterval(() => {
-  console.log('1 Hour Passed. Updating Clock');
-  updateClock(new Date().getHours());
-}, 3600000);
+// function updateClock(hour) {
+//   hourHand.classList.remove(hourHand.classList.item(1));
+//   const addClass = 'hour-' + ((hour > 12) ? (hour - 12).toString() : hour.toString());
+//   hourHand.classList.add(addClass);
+// }
 
-clockBtn.addEventListener('click', () => {
-  if(!stopped) {
-    stopped = true;
-    clearInterval(intervalId);    
-  } else {
-    updateClock(new Date().getHours());
-    setInterval(() => {
-      updateClock(new Date().getHours());
-    }, 3600000);
-  }
-});
+// updateClock(new Date().getHours());
+
+// let intervalId = setInterval(() => {
+//   console.log('1 Hour Passed. Updating Clock');
+//   updateClock(new Date().getHours());
+// }, 3600000);
+
+// clockBtn.addEventListener('click', () => {
+//   if(!stopped) {
+//     stopped = true;
+//     clearInterval(intervalId);    
+//   } else {
+//     updateClock(new Date().getHours());
+//     setInterval(() => {
+//       updateClock(new Date().getHours());
+//     }, 3600000);
+//   }
+// });
